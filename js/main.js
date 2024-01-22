5a5fef46020a47861057bd71a17d19c8b340cb7e
JeanCRuiz <139879016+JeanCRuiz@users.noreply.github.com>
2024-01-22 16:57:44 -0400

    Refactor task initialization and storage in main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { uniqueId, formatDate } from "./helpers"
 
-let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []
 
 const searchInput = document.querySelector('.search--input')
 const todoListContainer = document.querySelector('.todo--list')
@@ -29,6 +28,8 @@ const checkeIcon = `<svg
     d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"
     ></path>
     </svg>`;
+
+let tasks = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')) : []
 
 if (localStorage.getItem('tasks') === null) {
     localStorage.setItem('tasks', JSON.stringify(tasks))
